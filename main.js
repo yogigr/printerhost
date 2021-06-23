69ca02d6f7ce7c44dd78c6955d5b2f7d1c4e2d4a
Yogi Gilang Ramadhan <yogigilang182@gmail.com>
2021-06-24 00:00:30 +0700

    add icon & tray functionality

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu, dialog } = require('electron');
 const path = require('path');
 const url = require('url');
 
-let win
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
@@ -14,7 +14,8 @@ function createWindow () {
     height: 600,
     resizable: false,
     maximizable: false,
-
+    icon: path.join(__dirname, 'icon.ico'),
+    transparent: true,
     webPreferences: {
       //preload: path.join(__dirname, 'src/gui/index.js'),
       nodeIntegration: true, 
@@ -33,20 +34,52 @@ function createWindow () {
 
   // Open the DevTools.
   //win.webContents.openDevTools()
+
+  let tray = null;
+  win.on('minimize', function(event) {
+    event.preventDefault();
+    win.setSkipTaskbar(true);
+    tray = createTray();
+  })
+
+  win.on('restore', function(event) {
+    win.show();
+    win.setSkipTaskbar(false);
+    tray.destroy()
+  })
+
+  return win;
+}
+
+function createTray() {
+    let appIcon = new Tray(path.join(__dirname, "icon.ico"));
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Show', click: function () {
+                mainWindow.show();
+            }
+        },
+        {
+            label: 'Exit', click: function () {
+                app.isQuiting = true;
+                app.quit();
+            }
+        }
+    ]);
+
+    appIcon.on('double-click', function (event) {
+        mainWindow.show();
+    });
+    appIcon.setToolTip('Bams Printer Host');
+    appIcon.setContextMenu(contextMenu);
+    return appIcon;
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-
+  mainWindow = createWindow()
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -54,6 +87,12 @@ app.whenReady().then(() => {
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
--- src/gui/index.js
+++ src/gui/index.js
@@ -148,7 +148,37 @@ stopBtn.addEventListener('click', () => {
 
 //if test print btn click
 testPrintBtn.addEventListener('click', () => {
-    printer.print('Test Printing', printerName, {
+    //Set content to print...
+    //Create ESP/POS commands for sample label
+    var esc = '\x1B'; //ESC byte in hex notation
+    var newLine = '\x0A'; //LF byte in hex notation
+
+    var cmds = esc + "@"; //Initializes the printer (ESC @)
+    cmds += esc + '!' + '\x38'; //Emphasized + Double-height + Double-width mode selected (ESC ! (8 + 16 + 32)) 56 dec => 38 hex
+    cmds += 'BEST DEAL STORES'; //text to print
+    cmds += newLine + newLine;
+    cmds += esc + '!' + '\x00'; //Character font A selected (ESC ! 0)
+    cmds += 'COOKIES                   5.00'; 
+    cmds += newLine;
+    cmds += 'MILK 65 Fl oz             3.78';
+    cmds += newLine + newLine;
+    cmds += 'SUBTOTAL                  8.78';
+    cmds += newLine;
+    cmds += 'TAX 5%                    0.44';
+    cmds += newLine;
+    cmds += 'TOTAL                     9.22';
+    cmds += newLine;
+    cmds += 'CASH TEND                10.00';
+    cmds += newLine;
+    cmds += 'CASH DUE                  0.78';
+    cmds += newLine + newLine;
+    cmds += esc + '!' + '\x18'; //Emphasized + Double-height mode selected (ESC ! (16 + 8)) 24 dec => 18 hex
+    cmds += '# ITEMS SOLD 2';
+    cmds += esc + '!' + '\x00'; //Character font A selected (ESC ! 0)
+    cmds += newLine + newLine;
+    cmds += '11/03/13  19:53:17';
+
+    printer.print(cmds, printerName, {
         success: (jobId) => {
             pushLogs('Test Printing jobId: '+jobId)
             displayLogs()
